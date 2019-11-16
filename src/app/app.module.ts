1ea8c53d01f3a9771b6939fdc03968f0464a0b52
natanportilho <natanportilho@pop-os.localdomain>
2019-11-16 15:09:20 -0300

    Moving footer code into footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SortingFormComponent } from './components/sorting-form/sorting-form.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SortingFormComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
